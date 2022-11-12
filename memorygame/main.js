3eb3f29ed34b1d0d3a6f691e7e8261bb2733475a
Solomon Bestz <Kingsolodawarrior@gmail.com>
2022-11-12 18:27:56 +0100

    memory game

--- memorygame/main.js
+++ memorygame/main.js
@@ -17,7 +17,7 @@ const card_array = [
     },
     {
         name: 'beef-patty',
-        img: 'images/beef-patty',
+        img: 'images/beef-patty.jpg',
     },
     {
         name: 'tamales',
@@ -41,7 +41,7 @@ const card_array = [
     },
     {
         name: 'beef-patty',
-        img: 'images/beef-patty',
+        img: 'images/beef-patty.jpg',
     },
     {
         name: 'tamales',
@@ -54,13 +54,18 @@ card_array.sort(() => 0.5 - Math.random())
 
 
 const grid_display = document.querySelectorAll('.grid')
-console.log(grid_display)
+const result = document.querySelector('#result')
+let cards_chosen = []
+let cards_chosen_id = []
+let card_won = []
+
+
 function createboard(){
     for(let i = 0; i < card_array.length; i++){
         const card = document.createElement('img')
         card.setAttribute('src', 'images/hidden.png')
         card.setAttribute('data-id', i)
-        card.addEventListener( )
+        card.addEventListener('click', flipcard)
         grid_display[i].appendChild(card)
     }
 }
@@ -68,6 +73,48 @@ function createboard(){
 
 createboard()
 
-function flipcard(){
+function check_match(){
+    const card = document.querySelectorAll('.grid img')
+    const first_option = cards_chosen_id[0]
+    const second_option = cards_chosen_id[1]
 
+
+    console.log(card)
+    console.log('Check for match')
+
+    if (first_option === second_option){
+        alert('You clicked the same image')
+    }
+    if(cards_chosen[0] == cards_chosen[1]){
+        alert('You found a match')
+        card[first_option].setAttribute('src', 'images/check.png')
+        card[second_option].setAttribute('src', 'images/check.png')
+        card[first_option].removeEventListener('click', flipcard)
+        card[second_option ].removeEventListener('click', flipcard)
+        card_won.push(cards_chosen)
+    } else{
+         img_card[first_option].setAttribute('src', 'images/hidden.png')
+         img_card[second_option].setAttribute('src', 'images/hidden.png')
+         alert("try again")
+    }
+
+    cards_chosen = []
+    cards_chosen_id = []
+
+    if(card_won.length == (card_array.length / 2)){
+        result.innerHTML = "1"
+    }
+}
+
+function flipcard(){
+    console.log(card_array)
+    const card_id = this.getAttribute('data-id')
+    cards_chosen.push(card_array[card_id].name)
+    console.log(cards_chosen)
+    cards_chosen_id.push(card_id)
+    this.setAttribute('src', card_array[card_id].img)
+
+    if(cards_chosen.length === 2){
+        setTimeout(check_match, 500)
+    }
 }
